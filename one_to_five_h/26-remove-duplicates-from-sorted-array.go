a457ddb50b039187fab3678c1ded5af3956621c0
binnz <binnz>
2021-01-24 13:38:27 +0800

    26

---
+++ one_to_five_h/26-remove-duplicates-from-sorted-array.go
@@ -0,0 +1,13 @@
+package main
+
+func removeDuplicates(nums []int) int {
+	i, j := 0, 1
+	for j < len(nums) {
+		if nums[i] != nums[j] {
+			nums[i+1] = nums[j]
+			i++
+		}
+		j++
+	}
+	return i + 1
+}
